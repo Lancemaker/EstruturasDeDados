999cf200f6e7f6b83b331a10b658a4fad597880a
Gil <gil.felipe@foster.com.br>
2018-10-16 14:10:18 -0300

    that commit

--- pilhaDinamica/PilhaDinamica.java
+++ pilhaDinamica/PilhaDinamica.java
@@ -1,5 +1,9 @@
 package pilhaDinamica;
 
+// funciona como uma pilha, mas pode receber quantos elementos quiser. 
+//Precisa de nodes para guardar informações e fazer associalções entre objetos.
+
+
 public class PilhaDinamica {
 	private Node topo;
 	
@@ -8,7 +12,9 @@ public class PilhaDinamica {
 	}
 	
 	
-	public boolean isEmpty() {
+	public boolean isEmpty() { // se topo for == null significa que a variavel topo
+						       //nao aponta para nenhum objeto,
+						       //e isso significa que a pilha esta vazia.
 		return topo==null;
 	}
 	
@@ -28,11 +34,20 @@ public class PilhaDinamica {
 	
 	public void push(int elemento) {
 		Node novo= new Node(elemento);
-		if(topo != null) {
-			novo.proximo= topo;			
-		}
-		topo = novo;
+		if(topo != null) { //se a pilha nao estiver vazia.
+			novo.proximo= topo;	//aponta o proximo do novo para o topo.  		
+		}						//encadeando assim o novo antes do topo.
+		topo = novo; //se a pilha estiver vazia, simplesmente aponta topo ao novo.
 	}
 	
+	public int pop() {
+		Node retorno = topo; // aponta retorno para o topo.
+		if(!isEmpty()) { //se nao estiver vazio.
+			topo = topo.proximo;	// topo aponta pro seu proprio proximo, andando um pra trás.
+			retorno.proximo = null;  // faz o elemento se separar da cadeia associativa. 
+			return retorno.elemento; // retorna o elemento separado da cadeia.
+		}
+		return -1;
+	}
 	
 }
