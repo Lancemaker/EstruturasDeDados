58c02cd83a2e6701abfb8e7a6b75fbde774cd4c1
Gil <lancemaker@gmail.com>
2018-10-17 18:19:59 -0300

    that commit

--- filaDinamica/FilaDinamica.java
+++ filaDinamica/FilaDinamica.java
@@ -12,18 +12,43 @@ public class FilaDinamica {
 		return (inicio==null); // inicio ==null : true;
 	}
 	
-	public void enqueue(int elemento) {
+	public void enqueue(int elemento) { // insere no fim da fila.
 		Node novo = new Node(elemento);
 		
-		if(inicio==null) {
-			inicio = novo;
-			fim=inicio;
+		if(inicio==null) { //se estiver vazio. 
+			inicio = novo; //inicio recebe novo;
+			fim=inicio; 	// move o fim para o primeiro elemento,
+							// apontando para inicio.
 		}
 		else {
-			fim.next = novo;
-			fim= novo;
+			fim.next = novo; //encadeamos o next do ultimo elemnto ao novo.
+			fim= novo;	//movemos o fim para apontar para o ultimo elemento.
+		}		
+	}
+	
+	public int dequeue() { //remove e retorna o valor do primeiro elemento.
+		if(isEmpty())//se vazia retorna -1
+		return -1;
+		Node temp= inicio; //encadeia o primeiro elemento ao temp.
+		inicio = inicio.next; // aponta o inicio ao proximo do primeiro elemento.
+		// agora o temp tem o elemento retirado da primeira posicao. 
+		if(inicio == null) // se inicio apontar pra nulo
+			fim=null; // fim aponta pra nulo. 
+		return temp.elemento; //retorna o primeiro elemento. 
+	}
+	
+	public void display() {//printa 
+		if (isEmpty()) {
+			System.out.println("fila vazia");
 		}
-		
+		else {
+			Node atual = inicio;
+			while(atual!=null) {
+				System.out.println(atual.elemento);
+				atual=atual.next;
+			}
+			System.out.println();
+		}
 	}
 	
 }
