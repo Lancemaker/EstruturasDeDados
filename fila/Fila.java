11af366f05af0cf74d4cc1dd240296943a9c255f
Gil <gil.felipe@foster.com.br>
2018-10-17 09:33:37 -0300

    refactored

--- fila/Fila.java
+++ fila/Fila.java
@@ -26,7 +26,7 @@ public class Fila {
 		return quantidade;      //retorna o numero de elementos da fila. 
 	}
 	
-	public void enqueue(int elemento) {
+	public void enqueue(int elemento) {//adiciona um elemento a fila.
 		if(!isFull()) {
 			fim++;  	// se nao estiver lotado fim aponta pro proximo indice
 			fila[fim] = elemento; // preenche a fila no indice fim com o valor do elemento.
@@ -37,22 +37,27 @@ public class Fila {
 		}
 	}
 	
-	public int dequeue() {
-		if(!isEmpty()) {
-			int retorno = fila[inicio];
-			for(int i=0; i <fim; i++) {
-				fila[i]=fila[i+1];
+	public int dequeue() { // remove o primeiro elemento da fila e retorna o mesmo elemento.
+		if(!isEmpty()) { // se a fila nao estiver vazia. 
+			int retorno = fila[inicio]; // atribui ao retorno o valor do primeiro elemento. 
+			for(int i=0; i <fim; i++) { 
+				fila[i]=fila[i+1];//varre o array ate o indice do fim, 
+								 //substituindo o elmento atual pelo proximo.
 			}
-			fim--;
-			if(fim==-1) {
-				inicio--;
+			fim--; //decrementa o fim.
+			if(fim==-1) { //se o fim for -1, 
+				inicio--; //decrementa o inicio.
 			}
-			quantidade--;
-			return retorno;
+			quantidade--; // decrementa a quantidade.
+			return retorno; // retorna o elemento recem tirado da fila (o que era o primeiro.)
 		}
 		return -1;
 	}
 	
-	
-	
+	public int front() {
+		if(!isEmpty()) {
+			return fila[inicio]; //se nao estiver vazio, mostra o primeiro elemento da fila.
+		}
+		return-1; // se estiver vazio retorna -1.
+	}	
 }
